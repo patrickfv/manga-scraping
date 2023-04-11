3b4a7c092e0bc73914c1afb887d75bb85ce0653c
patrickfv <patrickfv.br@gmail.com>
2023-04-11 12:48:20 -0300

    refactor: add kwargs repository contract

--- data/contracts/load_manga_repository.py
+++ data/contracts/load_manga_repository.py
@@ -3,5 +3,5 @@ from data.models.manga import LoadMangaResult
 
 class LoadMangaRepository:
 
-    def load_manga(self, identifier: str) -> list[LoadMangaResult]:
+    def load_manga(self, name: str, **kwargs) -> list[LoadMangaResult]:
         pass
