cb144f85e84119350f9481f2e80ab30e5506b536
patrickfv <patrickfv.br@gmail.com>
2023-04-15 12:06:55 -0300

    refactor: add Chapter entity

--- data/models/manga.py
+++ data/models/manga.py
@@ -1,11 +1,16 @@
-from domain.entities.manga import Manga, Page
+from domain.entities.manga import Manga, Page, Chapter
 
 
 class PageParams(Page):
     pass
 
 
+class ChapterParams(Chapter):
+    def set_pages(self, page: PageParams | list[PageParams]):
+        super().set_pages(page)
+
+
 class LoadMangaResult(Manga):
-    def set_pages(self, pages: PageParams | list[PageParams]):
-        super().set_pages(pages)
+    def set_pages(self, chapter: ChapterParams | list[ChapterParams]):
+        super().set_chapters(chapter)
 
--- domain/entities/manga.py
+++ domain/entities/manga.py
@@ -1,27 +1,45 @@
 class Page:
-    def __init__(self, index: int, path: str):
+    def __init__(self, chapter, index: int, path: str):
+        self.chapter = chapter
         self.path = path
         self.index = index
 
 
+class Chapter:
+    pages: list[Page]
+
+    def __init__(self, manga, number: int, title: str, path: str):
+        self.manga = manga
+        self.number = number
+        self.title = title
+        self.path = path
+
+    def set_pages(self, page: Page | list[Page]):
+        if isinstance(page, Page):
+            self.pages.append(page)
+        if isinstance(page, list):
+            self.pages.extend(page)
+
+
 class Manga:
-    pages: list[Page] = []
+    chapters: list[Chapter] = []
     author: str = ''
     description: str = ''
-    categories: dict[str, str] = {}
+    categories: list = []
+    picture = None
 
     def __init__(self, name: str, path: str):
         self.name = name
         self.path = path
 
-    def get_pages(self):
-        return self.pages
+    def get_chapter(self):
+        return self.chapters
 
-    def set_pages(self, pages: Page | list[Page]):
-        if isinstance(pages, Page):
-            self.pages.append(pages)
-        if isinstance(pages, list):
-            self.pages.extend(pages)
+    def set_chapters(self, chapter: Chapter | list[Chapter]):
+        if isinstance(chapter, Chapter):
+            self.chapters.append(chapter)
+        if isinstance(chapter, list):
+            self.chapters.extend(chapter)
 
     def __str__(self):
         return '%s: %s' % (self.name, self.path)
