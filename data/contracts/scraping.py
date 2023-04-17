d0dc9cc1bdcfa7b95cc19e1777ebead9785695d5
patrickfv <patrickfv.br@gmail.com>
2023-04-16 23:50:43 -0300

    feat: find mangas mangahost repository

---
+++ data/contracts/scraping.py
@@ -0,0 +1,19 @@
+import urllib.request
+
+
+class RequestPage:
+    url = ''
+    header = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'
+    }
+
+    def get_doc_html(self):
+        req = urllib.request.Request(url=self.url, headers=self.header)
+        doc_html = urllib.request.urlopen(req).read()
+        return doc_html
+
--- infra/repositories/manga_host.py
+++ infra/repositories/manga_host.py
@@ -1,7 +1,23 @@
 from data.contracts.load_manga_repository import LoadMangaRepository
 from data.models.manga import LoadMangaResult
+from bs4 import BeautifulSoup
+from data.contracts.scraping import RequestPage
 
+class MangaHostRepository(LoadMangaRepository, RequestPage):
+    url = 'https://mangahosted.com/'
 
-class MangaHostRepository(LoadMangaRepository):
     def load_manga(self, name: str, **kwargs) -> list[LoadMangaResult]:
-        pass
+        self.url = '%sfind/%s' % (self.url, name.replace(' ', '+'))
+        doc_html = self.get_doc_html()
+        soup = BeautifulSoup(doc_html, 'html.parser')
+        tag_tr_list = soup.find('table', class_='table table-search table-hover').tbody.find_all('tr')
+        manga_list = []
+        for tag_result in tag_tr_list:
+            find = tag_result.find('a', class_='pull-left')
+            tag_a = find
+            describe = tag_tr_list[0].find('div', class_='entry-content').text
+            manga = LoadMangaResult(tag_a['title'], tag_a['href'])
+            manga.picture = tag_a.picture.source['srcset']
+            manga_list.append(manga)
+        return manga_list
+
